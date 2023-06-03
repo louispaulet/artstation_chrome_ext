94ba194cc56d4a9136775ef976a6746cd7ee4d53
louispaulet <louispaulet13@gmail.com>
2023-06-03 16:06:09 +0200

    also write title to description

--- artstation_chrome_ext/content.js
+++ artstation_chrome_ext/content.js
@@ -2,9 +2,14 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
   if (message.action === "fillField") {
     var inputField = document.querySelector('input[type="text"][name="title"]');
+    var textareaField = document.querySelector('textarea[name="description"]');
     if (inputField) {
       inputField.value = config.title;
       inputField.dispatchEvent(new Event('input', { bubbles: true })); // Trigger the 'input' event for validation
+    }
+    if (textareaField) {
+      textareaField.value = config.title;
+      textareaField.dispatchEvent(new Event('input', { bubbles: true })); // Trigger the 'input' event for validation
     }
   }
 });
